a9321bdd75d5e3c1c1d3c372b3c9ef2127379537
Vincent506 <vicenteneto0806@gmail.com>
2025-07-14 07:40:31 -0300

    add file

---
+++ Exception4/src/App.java
@@ -0,0 +1,16 @@
+import java.nio.file.FileAlreadyExistsException;
+
+public class App {
+
+    public class Verificar {
+    
+        public static void valiation() throws FileAlreadyExistsException{
+            
+        }
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
