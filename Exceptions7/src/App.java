3a34a169e3c459d572d0a00a49c7e9f9ede11f18
Vincent506 <vicenteneto0806@gmail.com>
2025-07-17 20:03:03 -0300

    Update

--- Exceptions7/src/App.java
+++ Exceptions7/src/App.java
@@ -1,20 +1,21 @@
-
+import java.util.InputMismatchException;
 
 public class App {
     public class Verfications{
         
-        public static void processar()throws Exception{
+        public static void processar()throws InputMismatchException{
             
                 try{
 
                 System.out.println("Criando um erro: ");
-                throw new Exception("Erro criado");
+                
+                throw new InputMismatchException("Erro criado");
                 }catch(Exception error){
                     System.out.println("Erro capturado: "+error.getMessage());
                 }
         }
     }
     public static void main(String[] args){
-
+        Verfications.processar();
     }
 }
