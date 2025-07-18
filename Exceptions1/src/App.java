3171bfbcd64dd465058a51f60610259aa048c664
Vincent506 <vicenteneto0806@gmail.com>
2025-07-17 21:09:39 -0300

    Update

--- Exceptions1/src/App.java
+++ Exceptions1/src/App.java
@@ -8,32 +8,45 @@ public class App {
             public static int dividir(int numerador, int denominador) 
             throws ArithmeticException
             {
+                if (denominador == 0) {
+                    throw new ArithmeticException("Você poderia digitar os numeros diferentes de 0? ");
+                }
             return numerador/denominador;
             }
+            public static void inteiros(String number) throws InputMismatchException{
+                if (!number.matches("\\d+")) {
+                    throw new InputMismatchException("Você poderia digitar os numeros inteiros?");
+                }
+            }
+            
     }
             public static void main(String[] args){
             Scanner input = new Scanner(System.in);
             boolean loop = true;
             do{
                 try{
+                    String variable1;
+                    String variable2;
                 System.out.println("Digite um numero inteiro: ");
-                int primeiro = Integer.parseInt(input.nextLine());
+                variable1 = input.nextLine();
+                DividirPorZero.inteiros(variable1);
+                int numerador = Integer.parseInt(variable1);
                 System.out.println("Digite outro numero inteiro: ");
-                int segundo = Integer.parseInt(input.nextLine());
-                int result = DividirPorZero.dividir(primeiro, segundo);
+                variable2 = input.nextLine();
+                DividirPorZero.inteiros(variable2);
+                int denominador = Integer.parseInt(variable2);
+                int result = DividirPorZero.dividir(numerador, denominador);
 
-                String mostrar = "O resultado da divisão entre o numero: "+primeiro
-                +" pelo "+segundo+" é "+result;
+                String mostrar = "O resultado da divisão entre o numero: "+numerador
+                +" pelo "+denominador+" é "+result;
 
                 System.out.println(mostrar);
                 loop = false;
                 }
                 catch (InputMismatchException exception){
-                    System.out.println("\nExceção:\n"+ exception.getMessage());
-                    System.out.println("Você poderia digitar os numeros novamente? ");
+                    System.out.println("\nExceção numero 1:\n"+ exception.getMessage());
                 }catch(ArithmeticException case0){
-                    System.out.println("\nExceção:\n"+case0.getMessage());
-                    System.out.println("Você poderia digitar os numeros novamente? ");
+                    System.out.println("\nExceção numero 2:\n"+case0.getMessage());
                 }
             }while(loop == true);
             input.close();
